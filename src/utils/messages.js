e72980577615e1635b884ef42a68da4eccd4c637
AFEDev <algirduj@gmail.com>
2022-06-18 23:09:23 +0300

    routes protection

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout': 'Now you are loged out',
+    'login' : 'Please login to system',
     'auth/wrong-password': 'Wrong password',
     'auth/user-not-found': 'User not found',
     'auth/email-already-in-use' : 'This email already registered'
