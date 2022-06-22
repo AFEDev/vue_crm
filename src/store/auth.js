110e46155ea0e03a614c1e02f54c40a6cb0e90c3
AFEDev <algirduj@gmail.com>
2022-06-22 10:54:52 +0300

    pretier, lithuanian language option

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,40 +1,44 @@
-import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 
 export default {
-    actions: {
-        async login({commit}, {email, password}) {
-            const auth = getAuth()
-            try {
-                await signInWithEmailAndPassword(auth, email, password)
-                }
-             catch (e) {
-                commit('setError', e)
-              throw e
-            }
-        },
-        async logout({commit}) {
-            await signOut(getAuth())
-            commit('clearInfo')
-        },
-        async register({commit},{email, password, name}) {
-            try {
-                await createUserWithEmailAndPassword(getAuth(), email, password)
-                    const uid = await dispatch('getUid')
-                    const db = getDatabase();
-                    set(ref(db, `users/${uid}/info`), {
-                        bill: 1000,
-                        name : name
-                    })
-        }
-            catch (e) {
-               commit('setError', e)
-                throw e
-            }
-        },
-    getUid() {
-        const user = getAuth().currentUser
-        return user ? user.uid : null
+  actions: {
+    async login({ commit }, { email, password }) {
+      const auth = getAuth();
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
     },
-}
-}
+    async logout({ commit }) {
+      await signOut(getAuth());
+      commit("clearInfo");
+    },
+    async register({ commit }, { email, password, name }) {
+      try {
+        await createUserWithEmailAndPassword(getAuth(), email, password);
+        const uid = await dispatch("getUid");
+        const db = getDatabase();
+        set(ref(db, `users/${uid}/info`), {
+          bill: 0,
+          name: name,
+          locale: "en-US",
+        });
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
+    getUid() {
+      const user = getAuth().currentUser;
+      return user ? user.uid : null;
+    },
+  },
+};
